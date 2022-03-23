74e4fa3d0da0f079a26d541d27eb822cbda56998
brahmed <medamine.brah@gmail.com>
2022-03-23 11:44:19 +0100

    Add page app bar custom widget.

---
+++ lib/widgets/page_app_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/arrow_icon.dart';
+
+class PageAppBar extends StatelessWidget {
+  final String title;
+
+  const PageAppBar({
+    required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      centerTitle: true,
+      title: Text(title, style: Theme.of(context).textTheme.headline1),
+      leading: GestureDetector(
+        onTap: () => Navigator.of(context).pop(),
+        child: const ArrowIcon(
+          icon: Icons.chevron_left,
+        ),
+      ),
+    );
+  }
+}
