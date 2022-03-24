f1a5c7d0dec03c5ac376e5cc38171a6cde97ac3d
brahmed <medamine.brah@gmail.com>
2022-03-24 13:20:27 +0100

    Add image url constants.

--- lib/config/images.dart
+++ lib/config/images.dart
@@ -1,3 +1,5 @@
 // App Images
 
 const String appImage = "assets/images/logo.png";
+const String googleLogo = "https://www.freepnglogos.com/uploads/google-favicon-logo-20.png";
+const String facebookLogo = "https://www.freepnglogos.com/uploads/facebook-logo-14.png";
