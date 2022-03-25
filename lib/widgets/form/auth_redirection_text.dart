549c0d0c2887c37b7463477a8eb249d9d7e34df2
brahmed <medamine.brah@gmail.com>
2022-03-25 15:14:09 +0100

    Add custom redirection text widget.

---
+++ lib/widgets/form/auth_redirection_text.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'gesture_text.dart';
+
+class AuthRedirectionText extends StatelessWidget {
+  final String staticText;
+  final String clickableText;
+  final String redirectionRouteName;
+
+  const AuthRedirectionText({
+    Key? key,
+    required this.staticText,
+    required this.clickableText,
+    required this.redirectionRouteName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "$staticText ",
+          textAlign: TextAlign.right,
+          softWrap: false,
+          style: Theme.of(context).textTheme.bodyText2,
+        ),
+        GestureText(
+          onTap: () =>
+              Navigator.of(context).pushReplacementNamed(redirectionRouteName),
+          text: clickableText,
+        ),
+      ],
+    );
+  }
+}
