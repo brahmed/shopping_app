7042452b007f51205f8685a55777903233419ce4
brahmed <medamine.brah@gmail.com>
2022-03-25 14:06:09 +0100

    Add gesture text custom widget.

---
+++ lib/widgets/gesture_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GestureText extends StatelessWidget {
+  final VoidCallback onTap;
+  final String text;
+  final TextStyle? textStyle;
+
+  const GestureText({
+    Key? key,
+    required this.onTap,
+    required this.text,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        text,
+        textAlign: TextAlign.right,
+        softWrap: false,
+        style: textStyle ??
+            theme.textTheme.bodyText1?.copyWith(color: theme.iconTheme.color),
+      ),
+    );
+  }
+}
