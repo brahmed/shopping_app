ceb8864a62eabd2cb9ae7705dd9a8f0d4d7b47c2
brahmed <medamine.brah@gmail.com>
2022-03-25 11:37:05 +0100

    Add app Localization.

---
+++ lib/config/localizations/application_localizations.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  /// Helper method to keep the code in the widgets concise
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  /// Static member to have a simple access to the delegate from the MaterialApp
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  Map<String, String>? _localizedStrings;
+
+  Future<bool> load() async {
+    /// Load the language JSON file from the "languages" assets folder
+    String jsonString = await rootBundle
+        .loadString('../assets/languages/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+
+    return true;
+  }
+
+  /// fetch translation files and translate by key
+  String? translate(String key) {
+    return _localizedStrings?[key];
+  }
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    /// supported language codes
+    return ['en', 'ar', 'fr'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    /// AppLocalizations class is where the JSON loading actually runs
+    AppLocalizations localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import '../config/app_theme.dart';
 import '../config/constants.dart';
@@ -8,25 +9,64 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // Using "static" so that we can easily access it later
-  static final ValueNotifier<ThemeData> themeNotifier =
-      ValueNotifier(AppTheme.light());
+  /// to change the app's language, this method needs to be called in the app language settings
+  static void setLocale(BuildContext context, Locale newLocale) async {
+    // get reference to state class
+    _MyAppState? state = context.findAncestorStateOfType<_MyAppState>();
+    // call change local helper method
+    state?.changeLanguage(newLocale);
+  }
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  /// Current locale
+  Locale? _locale;
+
+  /// Change language helper method
+  void changeLanguage(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<ThemeData>(
-      valueListenable: themeNotifier,
-      builder: (_, ThemeData currentTheme, __) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: kAppTitle,
-          theme: currentTheme,
-          initialRoute: Routes.tabs,
-          routes: Routes.routes(),
-        );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: kAppTitle,
+      theme: AppTheme.light(),
+      initialRoute: Routes.tabs,
+      routes: Routes.routes(),
+
+      // Current app local
+      locale: _locale,
+      // Localizations delegates
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      // Supported Locales
+      supportedLocales: const [
+        Locale('fr', 'FR'),
+        Locale('en', 'US'),
+        Locale('ar', 'TN'),
+      ],
+      // fetch current language values
+      localeResolutionCallback: (locale, supportedLocales) {
+        for (var supportedLocaleLanguage in supportedLocales) {
+          if (supportedLocaleLanguage.languageCode == locale?.languageCode &&
+              supportedLocaleLanguage.countryCode == locale?.countryCode) {
+            return supportedLocaleLanguage;
+          }
+        }
+        return supportedLocales.first;
       },
     );
   }
