de4074cdcd8c779b8c18f9e0f73e2707bc881540
brahmed <medamine.brah@gmail.com>
2022-03-27 14:20:02 +0200

    Add user token helpers.

---
+++ lib/utils/token_prefs_helpers.dart
@@ -0,0 +1,22 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Prefs user token key
+const String prefUserTokenKey = 'token';
+
+/// Save user token when logged in
+void saveUserToken(String token) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString(prefUserTokenKey, token);
+}
+
+/// Get user token
+Future<String?> getUserToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString(prefUserTokenKey);
+}
+
+/// Delete user token
+void deleteToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.remove(prefUserTokenKey);
+}
