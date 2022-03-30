10d39eadeade2070405644393d123b9e13402218
brahmed <medamine.brah@gmail.com>
2022-03-30 18:47:41 +0200

    Add search textField.

--- lib/screens/tab_pages/search_page.dart
+++ lib/screens/tab_pages/search_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../../config/colors.dart';
+import '../../../widgets/cards/app_card.dart';
+
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
@@ -8,12 +11,68 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  /// Search controller
+  final TextEditingController _searchController = TextEditingController();
+
+  /// Car code scan search result
+  String _scanBarcode = "";
+
+  /// On text changed
+  void _onTextChangedSearch(String text) {}
+
+  /// Reset search
+  void _cancelSearch() {
+    _searchController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text("Search Page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Search bar
+            AppCard(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /// Back arrow
+                  IconButton(
+                    onPressed: _cancelSearch,
+                    icon: const Icon(Icons.search_off_sharp),
+                  ),
+
+                  /// Search TextField
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      onChanged: _onTextChangedSearch,
+                      textInputAction: TextInputAction.search,
+                      decoration: const InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(color: ColorsSet.grayLight),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.all(4.0),
+                      ),
+                    ),
+                  ),
+
+                  /// Qrcode Search Icon
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.qr_code_scanner),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
