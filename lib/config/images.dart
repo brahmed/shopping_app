ad2212126426f4d0102ebbb7fb02248bbeeb1661
brahmed <medamine.brah@gmail.com>
2022-04-22 19:12:16 +0200

    Add empty screen image asset.

--- lib/config/images.dart
+++ lib/config/images.dart
@@ -1,5 +1,8 @@
 // App Images
-
 const String appImage = "assets/images/logo.png";
+const String emptyBoxImage = "assets/images/empty-box.svg";
+const String emptyShelfImage = "assets/images/empty-shelf.svg";
+
+// Icons
 const String googleLogo = "https://www.freepnglogos.com/uploads/google-favicon-logo-20.png";
 const String facebookLogo = "https://www.freepnglogos.com/uploads/facebook-logo-14.png";
